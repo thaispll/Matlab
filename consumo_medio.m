fa7fa713ebb852b1203d6c3ccded777bb3920aab
Thais Leal <58890658+thaispll@users.noreply.github.com>
2025-02-21 18:25:06 -0300

    Update

---
+++ consumo_medio.m
@@ -0,0 +1,28 @@
+distancia = -1
+litros = -1
+
+#loop para garantir que a quantidade seja um valor positivo
+while distancia <= 0
+  distancia = input('Digite a distância percorrida em km: ');
+  if distancia <= 0
+    fprintf('Por favor, insira um valor positivo para a distância!\n')
+  endif
+end
+
+#loop para garantir que a quantidade seja um valor positivo
+while litros <= 0
+  #solicito a qte de combustível consumido
+  litros = input('Digite a quantidade de combustível consumido em litros: ');
+  if litros <= 0
+    fprintf('Por favor, insira um valor positivo para a quantidade de combustível consumido!\n')
+  endif
+end
+
+#cálculo
+consumo_medio = distancia / litros;
+
+fprintf('O consumo médio é de %.2f km/l', consumo_medio)
+
+
+
+
---
+++ velocidade.m
@@ -0,0 +1,22 @@
+velocidades = [60, 75, 80, 90, 100]
+i = 1;
+
+while i<= length(velocidades)
+  if velocidades(i) <= 0
+    fprint('Erro: a velocidade não é válida. Todas as velocidades devem ser positivas. \n', velocidades(i));
+    return;
+  endif
+  i = i +1;
+end
+
+#calculo velocidade média
+velocidade_media = mean(velocidades);
+
+#calculo velocidade_maxima
+velocidade_maxima = max(velocidades);
+
+#exibo os resultados separadamente
+fprintf('A velocidade média é de %.2f km/h. \n', velocidade_media)
+fprintf('A velocidade máxima é de %d km/h. \n', velocidade_maxima)
+
+
